cc2fee2ab20b406bbacc55e0441ac083052e1691
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-03 21:31:26 +0200

    til glow

--- animation-assignment-2-2.js
+++ animation-assignment-2-2.js
@@ -1,7 +1,9 @@
 document.addEventListener('click', function(getMovementID){
     let movement = getMovementID.target.id;
     let ball = document.querySelector('img');
+    let bg = document.querySelector('div');
     ball.classList.add(movement);
+    bg.classList.add("bg"+movement);
     document.querySelector('p').innerHTML = "#" + movement;
 });
 
