755ee52bcfa23d03a800f86afe7d85d665b70505
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-04 08:43:25 +0200

    stop button added, other fixes til glow

--- animation-assignment-2-2.js
+++ animation-assignment-2-2.js
@@ -1,10 +1,8 @@
 document.addEventListener('click', function(elemID){
-    let movement = elemID.target.id;
+    let movement = elemID.target.id; //avoid repeat code to fire each animation
     let ball = document.querySelector('img');
     let background = document.querySelector('div');
     let shadow = document.querySelector('div.shadow');
-    ball.className = movement;
-    background.className = ("scene bg"+movement);
-    document.querySelector('p').innerHTML = "#" + movement;
+    ball.className = movement; // so when click another button, animation of that clicked button will start, overwrite the last clicked.
+    background.className = ("scene bg"+movement); //same as above. Also, trigger the whole set of animation(ball, background, shadow) at once
 });
-
