18011c254587c232e16677e86669bff486333658
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-04 10:25:46 +0200

    add bounce shadow and change horizon accordingly

--- animation-assignment-2-2.js
+++ animation-assignment-2-2.js
@@ -2,6 +2,8 @@ document.addEventListener('click', function(elemID){ //avoid repeat code to fire
     let movement = elemID.target.id;
     let ball = document.querySelector('img');
     let background = document.querySelector('.scene');
+    let shadow = document.querySelector('.shadow');
     ball.className = movement; // so when click another button, animation of the clicked button will start, overwrite the last one clicked.
     background.className = ("scene bg"+movement); //same as above. Also, trigger the whole set of animation(ball, background, shadow etc) at once
+    shadow.className = ("shadow shadow"+movement);
 });
