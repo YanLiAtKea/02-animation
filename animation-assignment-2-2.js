618c4f437d41e23a3c247b153969a75803e838a6
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-04 13:00:29 +0200

    10animations finished, JS screenshot

--- animation-assignment-2-2.js
+++ animation-assignment-2-2.js
@@ -1,9 +1,9 @@
-document.addEventListener('click', function(elemID){ //avoid repeat code to fire each animation /stop
-    let movement = elemID.target.id;
+document.addEventListener('click', function(movementID){ //avoid repeating code to fire each animation/stop
+    let movement = movementID.target.id;
     let ball = document.querySelector('img');
     let background = document.querySelector('.scene');
     let shadow = document.querySelector('.shadow');
-    ball.className = movement; // so when click another button, animation of the clicked button will start, overwrite the last one clicked.
-    background.className = ("scene bg"+movement); //same as above. Also, trigger the whole set of animation(ball, background, shadow etc) at once
-    shadow.className = ("shadow shadow"+movement);
+    ball.className = movement; // set className, so when click another button, animation of the clicked button will start, overwrite the last animation. // won't fire twice if click twice, cuz no change in class // not a toggle, so won't stop animation with second click. therefore add a stop button and set stop class to the original state of all elements, so one stop button controls all.
+    background.className = ("scene bg"+movement); //switch background with new click, same as above // set CSS class in this way, so one click can trigger the whole set of animations(ball, background, shadow etc)
+    shadow.className = ("shadow shadow"+movement); //same as above
 });
