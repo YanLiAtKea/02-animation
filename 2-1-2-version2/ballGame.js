de34ebbc2e54cd5d65a0daa0af45c482f5969c69
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-05 23:11:21 +0200

    add wind

--- 2-1-2-version2/ballGame.js
+++ 2-1-2-version2/ballGame.js
@@ -1,7 +1,17 @@
 document.addEventListener('click', function(movementID){
     let movement = movementID.target.id;
-    let ball = document.querySelector('img');
+    let ball = document.querySelector('#ball');
+    let ball2 = document.querySelector('#ball2');
+    let ball2Vertical = ball2.parentElement;
+    let ball2Horizontal = ball2Vertical.parentElement;
+    let ball3 = document.querySelector('#ball3');
     let background = document.querySelector('.scene');
     let shadow = document.querySelector('.shadow');
+    let wind = document.querySelector('.wind');
+    let blood = document.querySelector('.blood');
     ball.className = movement;
-    background.className = ("scene bg"+movement); shadow.className = ("shadow shadow"+movement); });
+    ball2Vertical.className = ("ball2Vertical" + movement);
+    ball2Horizontal.className = ("ball2Horizontal" + movement);
+    background.className = ("scene bg" + movement); shadow.className = ("shadow shadow" + movement);
+    wind.className = ("wind wind" + movement);
+});
