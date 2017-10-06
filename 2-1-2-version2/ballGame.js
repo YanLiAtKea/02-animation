d77fd5d4bc37c4e1b1fc9a950943cb4472351217
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-06 21:01:42 +0200

    add sound toggle

--- 2-1-2-version2/ballGame.js
+++ 2-1-2-version2/ballGame.js
@@ -2,10 +2,6 @@ let simpleAnimation = document.querySelector('.buttonAreaSimple');
 simpleAnimation.addEventListener('click', function (clickedElem) {
     let clicked = clickedElem.target.id;
     let ball = document.querySelector('#ball');
-    let ball2 = document.querySelector('#ball2');
-    let ball2Vertical = ball2.parentElement;
-    let ball2Horizontal = ball2Vertical.parentElement;
-    let ball3 = document.querySelector('#ball3');
     let background = document.querySelector('.scene');
     let shadow = document.querySelector('.shadow');
     let wind = document.querySelector('.wind');
@@ -13,11 +9,28 @@ simpleAnimation.addEventListener('click', function (clickedElem) {
     let allAudios = document.querySelectorAll('audio');
     let windAudio = document.querySelector('audio#wind');
     let ballAudio = document.querySelector('audio#bounce');
-
-    function ballBounceSound() {
-        ballAudio.play();
-        ballAudio.volume = .3;
-    };
+    let volumeOn = document.querySelector('#volumeOn');
+    let volumeOff = document.querySelector('#volumeOff');
+    // show/hide volume icon and toggle, call only when animation has sound
+    function toggleVolume() {
+        volumeOn.style.display = "block";
+        volumeOn.addEventListener('click', toggleVolumeOff);
+        function toggleVolumeOff() {
+            volumeOn.style.display = "none";
+            volumeOff.style.display = "block";
+            allAudios.forEach(function (playing) {
+                playing.muted = true;
+            });
+        }
+        volumeOff.addEventListener('click', toggleVolumeOn);
+        function toggleVolumeOn() {
+            volumeOn.style.display = "block";
+            volumeOff.style.display = "none";
+            allAudios.forEach(function (playing) {
+                playing.muted = false;
+            });
+        }
+    }
     // stop and reset all audio, so even click back this button, audio won't just resume
     function stopOtherAudio() {
         allAudios.forEach(function (playing) {
@@ -26,7 +39,13 @@ simpleAnimation.addEventListener('click', function (clickedElem) {
             playing.loop = false;
         });
     };
-    if (clicked !== "") { //remove the possibility to stop animation when user click anywhere on the page
+    // ball bounce sound, reusable
+    function ballBounceSound() {
+        ballAudio.play();
+        ballAudio.volume = .3;
+    };
+    //start animation
+    if (clicked !== "") { //remove the possibility to stop animation when user click anywhere on the page, like sound control, position control etc
         stopOtherAudio();
         let movement = clickedElem.target.id;
         ball.className = movement;
@@ -35,22 +54,25 @@ simpleAnimation.addEventListener('click', function (clickedElem) {
         wind.className = ("wind");
 
         if (movement == "moveFrom30") {
+            toggleVolume();
             stopOtherAudio();
             windAudio.play();
             wind.className = ("wind wind" + movement);
         } else if (movement == "oneJump") {
+            toggleVolume();
             stopOtherAudio();
             shadow.className = ("shadow shadow" + movement);
             setTimeout(ballBounceSound, 870);
             ballBounceSound;
             ballAudio.loop = false; // in case JUMP has set loop to true
         } else if (movement == "jump") {
+            toggleVolume();
             stopOtherAudio();
             shadow.className = ("shadow shadow" + movement);
             setTimeout(ballBounceSound, 500);
             ballAudio.playbackRate = 1.37;
             ballAudio.loop = true;
-        } else if (movement == "fade" || movement == "glow"){
+        } else if (movement == "fade" || movement == "glow") {
             background.className = ("scene bg" + movement);
         }
     }
